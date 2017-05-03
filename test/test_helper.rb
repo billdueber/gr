64eec2debb2063267f503338daa00ebb31896a1f
Bill Dueber <bill@dueber.com>
2017-05-02 21:55:28 -0400

    Skip all tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 require "minitest/autorun"
-require "minitest/reporters"
-MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
+require 'minitest/pride'
+
+
+#require "minitest/reporters"
+#MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 
 require_relative '../lib/gilded_rose'
 
