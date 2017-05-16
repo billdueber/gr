076abb84fc93c0cd3e7584ac51ecd597d8bccb8e
Seth A. Johnson <sethajoh@umich.edu>
2017-05-16 09:36:57 -0400

    initial commit

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,11 +7,21 @@ class GildedRose
     @name           = name
     @days_remaining = days_remaining
     @quality        = quality
+
+    if @quality > 50
+      # TODO: make better error message
+      puts "quality can't set over 50"
+      @quality = 50
+    end
   end
 
   def tick # one day passes
-
+    @days_remaining = @days_remaining - 1
+    if @name == "Aged Brie"
+      @quality = @quality + 1 unless @quality >= 50
+    else
+      @quality = @quality - 1 unless @quality <= 0
+    end
   end
 
 end
-
--- test/gilded_rose_test.rb
+++ test/gilded_rose_test.rb
@@ -14,4 +14,15 @@ describe "tick test" do
     @item.tick
     @item.days_remaining.must_equal 6
   end
+
+  it "quality can never be negative" do
+    @item.quality = 0
+    @item.tick
+    @item.quality.must_equal 0
+  end
+
+  it "quality can never be more than 50" do
+    @item = GildedRose.new("over50", 7, 51)
+    @item.quality.must_equal 50
+  end
 end
