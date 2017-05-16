10272893e4cda8a2912e8c717db9ca2efbcb7954
Jose Blanco <blancoj@umich.edu>
2017-05-16 09:53:55 -0400

    our test and code

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,7 +10,9 @@ class GildedRose
   end
 
   def tick # one day passes
-
+     @quality -=  1 if (@quality > 0 )
+     @quality = 50 if (@quality >= 49)
+     @days_remaining -= 1 
   end
 
 end
--- test/gilded_rose_test.rb
+++ test/gilded_rose_test.rb
@@ -14,4 +14,24 @@ describe "tick test" do
     @item.tick
     @item.days_remaining.must_equal 6
   end
+
+  it "quality does not go negative" do
+
+    digits = 0..100
+
+    digits.each do |digit|
+      @item.tick
+    end
+    @item.quality.must_equal 0
+  end
+
+    it "quality is more than 50" do
+
+    @item.quality = 102
+    @item.tick
+    @item.quality.must_equal 50
+  end
+
 end
+
+
