8e5e9c383e4be3e327649c548dfca957e124421f
Albert Bertram <bertrama@umich.edu>
2017-05-16 09:55:17 -0400

    Checked in.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,7 +10,12 @@ class GildedRose
   end
 
   def tick # one day passes
-
+    @days_remaining -= 1
+    if @name == 'Aged Brie'
+      @quality += 1 if @quality < 50
+    else
+      @quality -= 1 if @quality > 0
+    end
   end
 
 end
--- test/gilded_rose_test.rb
+++ test/gilded_rose_test.rb
@@ -3,6 +3,7 @@ require_relative 'test_helper.rb'
 describe "tick test" do
   before do
     @item = GildedRose.new("myitem", 7, 10)
+    @brie_item = GildedRose.new("Aged Brie", 7, 10)
   end
 
   it "has a quality which decreases" do
@@ -13,5 +14,20 @@ describe "tick test" do
   it "has a sell_in date which decreases" do
     @item.tick
     @item.days_remaining.must_equal 6
+  end
+
+  it "never has neg quality" do
+    1.upto(20) do @item.tick end;
+    @item.quality.must_equal 0
+  end
+
+  it "never more than 50 quality" do
+    1.upto(50) do @brie_item.tick end;
+    assert @brie_item.quality <= 50, "quality is #{@brie_item.quality}, expected 50"
+  end
+
+  it "increases with age if brie" do
+    @brie_item.tick
+    @brie_item.quality.must_equal 11
   end
 end
