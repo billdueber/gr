0504660342b2d7f755e340d1a6e65346b9fab676
gkostin1966 <gkostin@umich.edu>
2017-05-16 13:24:39 -0400

    case statement

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,50 +8,46 @@ class GildedRose
     @days_remaining = days_remaining
   end
 
-
   def tick
-    if name != 'Aged Brie' && name != 'Backstage passes to a TAFKAL80ETC concert'
-      if @quality > 0
-        if name != 'Sulfuras, Hand of Ragnaros'
-          @quality -= 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality += 1
-        if name == 'Backstage passes to a TAFKAL80ETC concert'
-          if @days_remaining < 11
-            if @quality < 50
-              @quality += 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality += 1
-            end
-          end
-        end
-      end
+    case name
+      when 'normal'
+        return normal_tick
+      when 'Aged Brie'
+        return brie_tick
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        return backstage_tick
+      when 'Sulfuras, Hand of Ragnaros'
+        return sulfuras_tick
     end
-    if name != 'Sulfuras, Hand of Ragnaros'
-      @days_remaining -= 1
-    end
-    if @days_remaining < 0
-      if name != "Aged Brie"
-        if name != 'Backstage passes to a TAFKAL80ETC concert'
-          if @quality > 0
-            if name != 'Sulfuras, Hand of Ragnaros'
-              @quality -= 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality += 1
-        end
-      end
-    end
+  end
+
+  def normal_tick
+    @days_remaining -= 1
+    return if quality == 0
+
+    @quality -= 1
+    @quality -= 1 if days_remaining <= 0
+  end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if quality == 50
+
+    @quality += 1
+    @quality += 1 if days_remaining <= 0
+  end
+
+  def sulfuras_tick
+    @days_remaining -= 1
+  end
+
+  def backstage_tick
+    @days_remaining -= 1
+    return if quality == 50
+    return @quality = 0 if days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if days_remaining < 10
+    @quality += 1 if days_remaining < 5
   end
 end
