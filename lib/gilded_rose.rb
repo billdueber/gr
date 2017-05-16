cd873e613cd3ef82692722f9a7759dd3dd61f8b7
Josh Steverman <josh@steverman.org>
2017-05-16 09:32:46 -0400

    initial commit

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,7 +10,18 @@ class GildedRose
   end
 
   def tick # one day passes
-
+    if @name == "Aged Brie" 
+      @quality += 1
+    else
+      @quality -= 1
+      if @quality < 0
+        @quality = 0
+      end
+    end
+    if @quality > 50
+     @quality = 50
+    end
+    @days_remaining -= 1
   end
 
 end
--- test/gilded_rose_test.rb
+++ test/gilded_rose_test.rb
@@ -3,6 +3,7 @@ require_relative 'test_helper.rb'
 describe "tick test" do
   before do
     @item = GildedRose.new("myitem", 7, 10)
+    @brie = GildedRose.new("Aged Brie", 7, 10)
   end
 
   it "has a quality which decreases" do
@@ -14,4 +15,21 @@ describe "tick test" do
     @item.tick
     @item.days_remaining.must_equal 6
   end
+
+  it "quality is never negative" do
+    (1..20).each {@item.tick} 
+    @item.quality.must_equal 0
+  end
+
+  it "increases the quality of Aged Brie" do
+    @brie.tick
+    @brie.quality.must_equal 11
+  end
+
+  it "quality is never more than 50" do
+    (1..50).each {@brie.tick}
+    @brie.quality.must_equal 50
+  end
+
+  
 end
