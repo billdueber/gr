aaaa07f710cfa256f7e150959650310622363904
Matthew Alexander LaChance <mattlach@umich.edu>
2017-05-16 09:31:15 -0400

    :white_check_mark: It does the stuff you said

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,7 +10,30 @@ class GildedRose
   end
 
   def tick # one day passes
+    if self.we_increase_in_quality_over_time
+      self.increase_quality
 
+    else
+      self.decrease_quality
+    end
+
+    @days_remaining -= 1
+  end
+
+  def we_increase_in_quality_over_time
+    @name == "Aged Brie"
+  end
+
+  def increase_quality
+    unless @quality == 50
+      @quality += 1
+    end
+  end
+
+  def decrease_quality
+    unless @quality == 0
+      @quality -= 1
+    end
   end
 
 end
--- test/gilded_rose_test.rb
+++ test/gilded_rose_test.rb
@@ -5,13 +5,58 @@ describe "tick test" do
     @item = GildedRose.new("myitem", 7, 10)
   end
 
-  it "has a quality which decreases" do
-    @item.tick
-    @item.quality.must_equal 9
+  it 'cannot have a negative quality' do
+    20.times {@item.tick}
+    @item.quality.must_equal 0
   end
 
-  it "has a sell_in date which decreases" do
+  describe "after one tick" do
+    before do
+      @item.tick
+    end
+
+    it "has a quality which decreases" do
+      @item.quality.must_equal 9
+    end
+
+    it "has a sell_in date which decreases" do
+      @item.days_remaining.must_equal 6
+    end
+
+    describe "then a second tick" do
+      before do
+        @item.tick
+      end
+
+      it "has a quality which decreases again" do
+        @item.quality.must_equal 8
+      end
+
+      it "has a sell_in date which decreases again" do
+        @item.days_remaining.must_equal 5
+      end
+    end
+  end
+end
+
+describe "aged brie" do
+  before do
+    @item = GildedRose.new("Aged Brie", 7, 10)
+  end
+
+  it "increases in quality over time" do
     @item.tick
-    @item.days_remaining.must_equal 6
+    @item.quality.must_equal 11
+  end
+
+  it "increases in quality over more time" do
+    @item.tick
+    @item.tick
+    @item.quality.must_equal 12
+  end
+
+  it "doesn't increase past 50" do
+    50.times {@item.tick}
+    @item.quality.must_equal 50
   end
 end
